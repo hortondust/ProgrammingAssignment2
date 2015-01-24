53502b306506bcd4b8b33dffe51accab1e89179c
Dustin Horton <hortondust@gmail.com>
2015-01-24 17:13:53 -0500

    filling in the cachable matrix inverse functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
+#functions for computing the inverse of a matrix, and caching the inverse to save computations if requested again.
 
-## Write a short comment describing this function
-
+## Creates a inverse-caching matrix from the given input matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+  solved <- NULL
+  set <- function(y) {
+    x <<- y
+    solved <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) solved <<- solve
+  getsolve <- function() solved
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
